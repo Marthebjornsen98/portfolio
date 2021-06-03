293ef53e3bd40bfee0e0770d61a17af0f25d5552
Marthe Bjornsen <69841912+Marthebjornsen98@users.noreply.github.com>
2021-06-03 22:12:30 +0200

    trying to make a slider :)))

--- js/script.js
+++ js/script.js
@@ -16,12 +16,50 @@ for (let i = 0; i < navItem.length; i++) {
     };
 };
 
-// Changing background-color when you scroll past the hero banner
-window.onscroll = function () {
-    if (window.scrollY > 800) {
-        header.style.backgroundColor = 'var(--white)';
+$(function () {
+    $('.responsive').slick({
+        infinite: true,
+        dots: true,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
 
-    } else {
-        header.style.backgroundColor = 'var(--transparant)';
-    };
-};
+slider()
+
+// Changing background-color when you scroll past the hero banner
+// window.onscroll = function () {
+//     if (window.scrollY > 800) {
+//         navList.style.backgroundColor = 'var(--white)';
+
+//     } else {
+//         navList.style.backgroundColor = 'var(--transparant)';
+//     };
+// };
